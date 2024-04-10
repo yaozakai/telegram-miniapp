74f2e43449e07c2c5aad6f22e669be09f7c2a793
Walt <walt.yaoza@gmail.com>
2024-04-10 01:29:13 -0700

    ping 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Do the salsa ${name}!`);
+  res.send(`Do th salsa ${name}!`);
 });
 
 const port = parseInt(process.env.PORT) || 3000;
